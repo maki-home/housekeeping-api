4d67ef79e9f2390ba6c289cdabde3197829e97c8
Toshiaki Maki <tmaki@pivotal.io>
2020-06-25 16:38:44 +0900

    Skip integration test

--- src/test/java/am/ik/home/housekeeping/HousekeepingApiApplicationTests.java
+++ src/test/java/am/ik/home/housekeeping/HousekeepingApiApplicationTests.java
@@ -3,7 +3,7 @@ package am.ik.home.housekeeping;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class HousekeepingApiApplicationTests {
 
     @Test
